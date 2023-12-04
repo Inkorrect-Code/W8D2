3b91fcb833eaaac0edd5c2aeb037e68b1d379aeb
Inkorrect-Code <ntongprofessional@gmail.com>
2023-12-03 21:40:25 -0500

    working on phase 2

--- practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -14,3 +14,25 @@ Array.prototype.uniq = function(){
 
 //console.log([1,2,2,3,3,3].uniq()) // => [1,2,3]
 
+// ================
+
+Array.prototype.twoSum = function(){
+    const resultArray = [];
+
+    for (let i = 0; i < this.length; i++){
+        for (let j = 0; j < this.length; j++){
+            if (i < j){
+                if (this[i] + this[j] === 0){
+                    resultArray.push([i, j])
+                };
+            };
+        };
+    };
+    return resultArray
+}
+
+// console.log([-1, 0, 2, -2, 1].twoSum()) // => [[0,4] [2,3]]
+
+Array.prototype.transpose = function(){
+
+}

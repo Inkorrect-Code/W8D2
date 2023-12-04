d5987c05dbc354cfc2e0581617e8321dae7b2702
Inkorrect-Code <ntongprofessional@gmail.com>
2023-12-03 21:58:33 -0500

    working on phase 2

--- practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -34,5 +34,17 @@ Array.prototype.twoSum = function(){
 // console.log([-1, 0, 2, -2, 1].twoSum()) // => [[0,4] [2,3]]
 
 Array.prototype.transpose = function(){
+    const resultArray = [];
 
+    for (let col = 0; col < this[0].length; col++){
+        const transposed = [];
+
+        for (let row = 0; row < this.length; row++){
+            transposed.push(this[row][col])
+        };
+        resultArray.push(transposed);
+    };
+    return resultArray;
 }
+
+console.log([[0, 1, 2], [3, 4, 5], [6, 7, 8]].transpose()) // => [[0,3,6], [1,4,7], [2,5,8]]

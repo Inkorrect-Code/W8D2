2cb7fe7f52e473ff3d533d4e659093fa9bc6d784
Jacob Tabor <jtabor214@gmail.com>
2023-12-05 16:11:38 -0500

    Moved input from phase-1.js to phase-2.js and completed .uniq in phase-2

--- practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,39 +1,16 @@
-//  dont need to open up the array class to enter it to MP
-Array.prototype.myEach = function(proc){
-    // 'this' is the equiv to 'self' in ruby
-    //  apply this array to every index
-    //  'i++' is the same as i += 1 (some nuances but for now yeah)
-    // ( ) in a way rep the start and end parts of a ruby function
-    // 'callback' is the same idea as a 'proc' in ruby ==> pass in another function to this function
-    for (let i = 0; i < this.length; i++){
-        let elem = this[i] // gives each elem at each index
-        proc(elem)
-    }
-    return this
-}
-// this is the callback being passed in
-//  note that this can be created outside of the function it is passed into
-function print_statement(elem){
-    console.log(elem);
-}
-
-// let arr1 = ['a', 'b', 'c']
-// arr1.myEach(print_statement)
-// =========================================================================
-
-Array.prototype.myMap = function(callback) {
+Array.prototype.uniq = function(){
     const resultArray = [];
-    
-    this.myEach(function(elem) {
-        resultArray.push(callback(elem));
-    });
 
-    // console.log(resultArray)
-    return resultArray;
+    for (let i = 0; i < this.length - 1; i++){
+        if (!resultArray.includes(this[i])) {
+            resultArray.push(this[i]);
+        };
+        
+    };
+
+    return resultArray
+
 }
 
-function callback(elem){
-    return(elem + "!") 
-}
-let arr1 = ['a', 'b', 'c']
-console.log(arr1.myMap(callback))
+//console.log([1,2,2,3,3,3].uniq()) // => [1,2,3]
+
